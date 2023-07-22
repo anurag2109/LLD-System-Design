e381aa1675fe8070d5ad89c83e0e63efaae37c8f
Admin <Admin@192.168.1.4>
2023-07-22 19:07:45 +0530

    Notify Me (Observer & Observable Design pattern)

--- Decorator_design_pattern_PizzaCost/src/main/java/com/LLD/Decorator_design_pattern_PizzaCost/model/basePizza/BasePizza.java
+++
@@ -1,5 +0,0 @@
-package com.LLD.Decorator_design_pattern_PizzaCost.model.basePizza;
-
-public abstract class BasePizza {
-	public abstract int cost();
-}
--- Decorator_design_pattern_PizzaCost/src/main/java/com/LLD/Decorator_design_pattern_PizzaCost/model/decorator/ToppingDecorator.java
+++
@@ -1,7 +0,0 @@
-package com.LLD.Decorator_design_pattern_PizzaCost.model.decorator;
-
-import com.LLD.Decorator_design_pattern_PizzaCost.model.basePizza.BasePizza;
-
-public abstract class ToppingDecorator extends BasePizza {
-	
-}
